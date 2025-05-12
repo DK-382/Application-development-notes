f0013228395df998820d9e357b64c35c500419b9
tmxk2018 <19825167213@139.com>
2025-05-12 16:04:32 +0800

    overAB

---
+++ web_http/app.py
@@ -0,0 +1,12 @@
+# app.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello from Flask'
+
+app.run(host='0.0.0.0', port=8090)
+
+
+# .\filebrower.exe -p 8090
